54301a5dcedd61fc7fa4ebb7b98c5d2000c17352
moradi62 <mm62@outlook.com>
2022-09-08 10:41:36 +0300

    2-ой способ

--- 3_4.py
+++ 3_4.py
@@ -1,12 +1,27 @@
 # Пользователь вводит 3 числа, сказать сколько из них положительных
 # и сколько отрицательных
 
-num_1 = input('Number 1: ')
-num_2 = input('Number 2: ')
-num_3 = input('Number 3: ')
+# num_1 = input('Number 1: ')
+# num_2 = input('Number 2: ')
+# num_3 = input('Number 3: ')
+#
+# res_zero = int(num_1) !=0
+# res_minus = int(num_1.startswith('-') + num_2.startswith('-') + num_3.startswith('-'))-res_zero
+# res_plus = 3 - int(res_minus)
+#
+# print('Количество отрицательных чисел: ', res_minus)
+# print('Количество положительных чисел: ', res_plus)
+# print('Количество 0: ', res_zero)
 
-res_minus = num_1.startswith('-') + num_2.startswith('-') + num_3.startswith('-')
-res_plus = 3 - res_minus
+#Второй вариант
+num_1 = int(input('Number 1: '))
+num_2 = int(input('Number 2: '))
+num_3 = int(input('Number 3: '))
+
+res_zero = (num_1 == 0) + (num_2 == 0) + (num_3 == 0)
+res_plus = (num_1 > 0) + (num_2 > 0) + (num_3 > 0)
+res_minus = 3-res_plus-res_zero
 
 print('Количество отрицательных чисел: ', res_minus)
 print('Количество положительных чисел: ', res_plus)
+print('Количество 0: ', res_zero)

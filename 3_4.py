213236de900c82009944c4507f4bfa5c57f26c6c
moradi62 <mm62@outlook.com>
2022-09-08 10:57:51 +0300

    2-ой способ

--- 3_4.py
+++ 3_4.py
@@ -1,17 +1,17 @@
 # Пользователь вводит 3 числа, сказать сколько из них положительных
 # и сколько отрицательных
 
-# num_1 = input('Number 1: ')
-# num_2 = input('Number 2: ')
-# num_3 = input('Number 3: ')
-#
-# res_zero = int(num_1) !=0
-# res_minus = int(num_1.startswith('-') + num_2.startswith('-') + num_3.startswith('-'))-res_zero
-# res_plus = 3 - int(res_minus)
-#
-# print('Количество отрицательных чисел: ', res_minus)
-# print('Количество положительных чисел: ', res_plus)
-# print('Количество 0: ', res_zero)
+num_1 = input('Number 1: ')
+num_2 = input('Number 2: ')
+num_3 = input('Number 3: ')
+
+res_zero = int(num_1) !=0
+res_minus = int(num_1.startswith('-') + num_2.startswith('-') + num_3.startswith('-'))-res_zero
+res_plus = 3 - int(res_minus)
+
+print('Количество отрицательных чисел: ', res_minus)
+print('Количество положительных чисел: ', res_plus)
+print('Количество 0: ', res_zero)
 
 #Второй вариант
 num_1 = float(input('Number 1: '))

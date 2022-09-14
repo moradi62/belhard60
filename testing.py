73a438c0312c6e6a43cb63732e0ae37fc12dce91
moradi62 <mm62@outlook.com>
2022-09-14 17:10:19 +0300

    main 14092022 - office

--- testing.py
+++ testing.py
@@ -2,6 +2,7 @@
 # for i in range(2):
 #     for j in range(3):
 #         print(matrix[i][j], end = '  ')
+import setuptools
 
 # from turtle import *
 # color('red', 'yellow')
@@ -13,6 +14,27 @@
 #         break
 # end_fill()
 # done()
-a , b = 'a', 5
-print(a)
-print(b)
+# a = 'mohsen"s %s Moradi' %'Ali'
+# S = 'sypatex'
+# print("a {0} parrot".format(kind)S.find('pa'))
+# print(a)
+# ss = 'sdfsdfsdf' 'ghhjghj' "hjgj"
+# print(ss)
+# aa = [1,2,3,4,5,6,7,8,9]
+# bb = []
+# cc = []
+# print(len(aa))
+# bb.append(aa[i] for i in range(0,len(aa)-1))
+# cc.__add__(aa[i] for i in range(0,len(aa)-1))
+# print(bb)
+# print(cc)
+# num_str = '123456789'
+# # aa = [a for a in num_str if (a != '0' and b != '0')]
+# aa = []
+# # aa.insert(1, num_str[int(j)] for i in num_str for j in range(0,len(num_str)-1))
+# print(num_str[1])
+# bb = aa.insert(2, num_str[3])
+#
+# print(aa)
+print('\x45')
+print('\u0105')

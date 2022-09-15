7acda916d1688663149e05602dc82afe6f89a25c
moradi62 <mm62@outlook.com>
2022-09-15 10:19:08 +0300

    Домашнее задание 4 урок (версия 0.0001)

--- 4_2.py
+++ 4_2.py
@@ -2,16 +2,13 @@
 # создавать словарь для подсчитывания количества вхождений каждой
 # буквы в текст введенный с клавиатуры
 
-# alpha = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ',
-#          '0','1','2','3','4','5','6','7','8','9'}
-dict = {}
-words = input('Enter your text: ')
+dic = {}
 
-# for i in alpha:
-#     if words.count(i) > 0: print('Количества вхождений буква ', i, 'в тексте = ', words.count(i))
+words = input('Enter your text: ').islower()
 
 for i in words:
     if i == ' ':
         continue
-    dict[i] = words.count(i)
-print('Словарь вхождении буква :\n', dict)
+    dic[i] = words.count(i)
+
+print('Словарь вхождении буква :\n', dic)

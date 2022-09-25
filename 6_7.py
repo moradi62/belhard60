05f06789944c0056e95115d2d04a1184491fcad7
moradi62 <mm62@outlook.com>
2022-09-25 18:21:48 +0300

    Home 25092022

--- 6_7.py
+++ 6_7.py
@@ -1,3 +1,23 @@
-Дан список чисел, необходимо для каждого элемента посчитать сумму его
-соседей, для крайних чисел одним из соседей является число с противоположной
-стороны списка
+# Дан список чисел, необходимо для каждого элемента посчитать сумму его
+# соседей, для крайних чисел одним из соседей является число с противоположной
+# стороны списка
+
+from random import randint
+
+
+def sum_lst(lst: list)->list:
+    buff_lst = []
+    buff_lst.append(lst[0]+lst[1]) # index = 0
+    for ind in range(1, len(lst)-1): # index 1 ~ n-1
+        buff_lst.append(lst[ind-1]+lst[ind+1])
+    buff_lst.append(lst[-2]+lst[-1])
+    return buff_lst
+
+lst = []
+for i in range(10):
+    lst.append(randint(0, 10))
+
+print(lst)
+
+print(sum_lst(lst))
+

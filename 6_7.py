f8c6567b1b08070ccb69942add24e8fb94f6d6d7
moradi62 <mm62@outlook.com>
2022-09-26 17:28:26 +0300

    Домашнее задание 6 урок (версия 0.0001)

--- 6_7.py
+++ 6_7.py
@@ -1,3 +1,31 @@
-Дан список чисел, необходимо для каждого элемента посчитать сумму его
-соседей, для крайних чисел одним из соседей является число с противоположной
-стороны списка
+# Дан список чисел, необходимо для каждого элемента посчитать сумму его
+# соседей, для крайних чисел одним из соседей является число с противоположной
+# стороны списка
+
+from random import randint
+
+def sum_num(lst:list) -> list:
+    res_lst = []
+    for ind in range(0, len(lst)):
+        if ind == 0:
+            res_lst.append(lst[-1] + lst[1])
+        elif ind == len(num_lst) - 1:
+            res_lst.append(lst[0] + lst[-2])
+        else:
+            res_lst.append(lst[ind - 1] + lst[ind + 1])
+    return res_lst
+
+
+num_lst = []
+
+for rnd in range(0,10):
+    num_lst.append(randint(0, 10))
+
+print('список рандомных чисел')
+print(num_lst)
+print('сумма крайних чисел')
+print(sum_num(num_lst))
+
+
+
+
--- 6_8.py
+++ 6_8.py
@@ -1,2 +1,15 @@
-Дан словарь, ключ - Название страны, значение - список городов, на вход
-поступает город, необходимо сказать из какой он страны
+# Дан словарь, ключ - Название страны, значение - список городов, на вход
+# поступает город, необходимо сказать из какой он страны
+
+country = {'Belarus':['Minsk', 'Grodno', 'Vitebsk', 'Brest', 'Mogiliev'],
+           'USA':['NY', 'LA', 'Chicago', 'California'],
+           'Japan':['Yokohama', 'Osaka', 'Nagoya']}
+
+src = input('Search your City: ')
+
+for k, v in country.items():
+    if src in v:
+        print(f'{k}')
+
+
+
